6fdde99392eab130b39998777a527da3635553ab
Nathaniel Bethea <jumpnate23@outlook.com>
2022-03-07 09:16:48 -0800

    week-3-assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
+const { it, expect } = require("@jest/globals");
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -9,7 +11,6 @@
 
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
-
 
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
@@ -21,36 +22,123 @@
 // Example input: 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+// Pseudo Code
+// create a test for the expected outputs
+// create a describe statement with new function name
+// create let statements for each example inputs
+// create an it statement that states the purpose of our function
+// create expect statements for each example outputs
+
+describe("fibSeq", () => {
+  let newSeq = [1, 1, 2, 3, 5, 8];
+  let fullerSeq = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+  it("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence", () => {
+    expect(fibSeq(6)).toEqual([1, 1, 2, 3, 5, 8]),
+      expect(fibSeq(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55]);
+  });
+});
 
 // b) Create the function that makes the test pass.
 
+// Pseudo Code
+// create a const for you above describe statement
+// assign newArr as a variable
+// create a for loop
+// use .push to add newArr
+// create return for newArr
+
+const fibSeq = (num) => {
+  let newArr = [1, 1];
+  for (let i = 1; i < num - 1; i++) {
+    newArr.push(newArr[i] + newArr[i - 1]);
+  }
+  return newArr;
+};
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"];
 // Expected output: [-9, 7, 9, 199]
 
-const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"];
 // Expected output: [-823, 7, 23]
 
+// Pseudo Code
+// create a test for the expected outputs
+// create a describe statement with new function name
+// create let statements for each example inputs
+// create an it statement that states the purpose of our function
+// create expect statements for each example outputs
+
+describe("newArr", () => {
+  let fullArr = [-9, 7, 9, 199];
+  let fullerArr = [-823, 7, 23];
+  it("pulls specific values from the above constants", () => {
+    expect(newArr(fullArr1)).toEqual(fullArr);
+    expect(newArr(fullArr2)).toEqual(fullerArr);
+  });
+});
 
 // b) Create the function that makes the test pass.
 
+// Pseudo Code
+// create a function named newArr
+// use .filter calling upon a value
+// return only odd numbers
+// sort arrays a and b
+
+const newArr = (array) => {
+  let result = array.filter((value) => {
+    return typeof value === "number" && value % 2 !== 0;
+  });
+  return result.sort((a, b) => a - b);
+};
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
+const numbersToAdd1 = [2, 4, 45, 9];
+// Expected output: [2, 6, 51, 60]
 
-const numbersToAdd2 = [0, 7, -8, 12]
+const numbersToAdd2 = [0, 7, -8, 12];
 // Expected output: [0, 7, -1, 11]
 
-const numbersToAdd3 = []
+const numbersToAdd3 = [];
 // Expected output: []
 
+// Pseudo Code
+// create a test for the expected outputs
+// create a describe statement with new function name
+// create let statements for each example inputs
+// create an it statement that states the purpose of our function
+// create expect statements for each example outputs
+
+describe("accSum", () => {
+  let numbAdd1 = [2, 6, 51, 60];
+  let numbAdd2 = [0, 7, -1, 11];
+  let numbAdd3 = [];
+  it("takes in an array and returns an array of the accumulating sum. An empty array should return an empty array", () => {
+    expect(accSum(numbersToAdd1)).toEqual([2, 6, 51, 60]);
+    expect(accSum(numbersToAdd2)).toEqual([0, 7, -1, 11]);
+    expect(accSum(numbersToAdd3)).toEqual([]);
+  });
+});
 
 // b) Create the function that makes the test pass.
+
+// Pseudo Code
+// create a const for accSum
+// create a variable for newSum
+// create a return using .map and a function for our value
+// return our newSum
+
+const accSum = (array) => {
+  let newSum = 0;
+  return array.map((value) => {
+    return (newSum += value);
+  });
+  return newSum;
+};
--- interview-questions.md
+++ interview-questions.md
@@ -6,62 +6,50 @@ First, without external resources. Challenge yourself to answer from memory as i
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
-
 1. What is `this` in JavaScript?
 
-  Your answer:
+Your answer: an object
 
-  Researched answer:
-
-
+Researched answer: an object carrying out a specific piece of code
 
 2. What is React? Why would you use it?
 
-  Your answer:
+Your answer: A built in library where users can create applications
 
-  Researched answer:
-
-
+Researched answer: An open-source library within JavaScript that allows you to build apps
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+Your answer: Mounting
 
-  Researched answer:
-
-
+Researched answer: render()
 
 4. What is JSX? What is one notable difference between HTML and JSX?
 
-  Your answer:
+Your answer: JavaScript Syntax Extension, both are a type of code
 
-  Researched answer:
-
-
+Researched answer: JavaScript Syntax Extension, JSX was created for REACT
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+Your answer: A package manager, whichever file you are running
 
-  Researched answer:
-
-
+Researched answer: A package manager for node.js, and node_modules files are modified
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+Your answer: a function that is not directly called upon
 
-  Researched answer:
-
+Researched answer: a function that does not have a name
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: The action of being able to control what is displayed
 
-2. Props:
+2. Props: Arguments that get passed in to components
 
-3. JavaScript Events:
+3. JavaScript Events: JavaScripts method of how interactions happen with HTML
 
-4. Object-oriented programming:
+4. Object-oriented programming: A model that organizes software before logic and function
 
-5. Ruby:
+5. Ruby: A language created for OOP and known for its simplicity and ability to be brought up in the terminal
